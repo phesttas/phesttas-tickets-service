c3cc59a5d7bcf303c75e754e244b760f5f686d18
Fernando Passos <fernandopassoz@gmail.com>
2022-06-21 19:52:00 -0300

    gin added

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
 import "fmt"
+import "github.com/gin-gonic/gin"
+
+func handleTest(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"I'm": "here!",
+	})
+}
 
 func main() {
 	fmt.Println("Starts here!")
+	r := gin.Default()
+	r.GET("/", handleTest)
+	r.Run()
 }
